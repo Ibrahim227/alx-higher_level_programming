83b00e04a179c2ff5190780f1fcbd52d29687038
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-11-27 13:09:09 -0800

    add and update

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* check_cycle - check cycle
+* @list: a pointer to check the list
+* Return: 0 if no cycle, 1 otherwise
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
+
