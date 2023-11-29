8d59f3a24a4745e9918da19eba14cc46184dabb5
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-11-29 03:15:01 -0800

    update 9-lastnumer

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        ld = number % -10
+        ld = (-number % 10)
     elif number >= 0:
         ld = number % 10
     print("{:d}".format(ld), end="")
