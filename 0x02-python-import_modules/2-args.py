5bc0b044613c1462bffa6eb90b9923a696b3cbd1
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-11-30 15:57:14 -0800

    update 2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    """import sys
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
@@ -9,4 +9,11 @@ if __name__ == "__main__":
     else:
         print("{} arguments".format(count))
     for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))"""
+    from sys import argv
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    plural_s = "s" if argc != 1 else ""
+    print("{} argument{}{}{}".format(argc, plural_s, ":"if argc > 0 else ".", ""))
+    for i, arg in enumerate(argv[1:], start=1):
+        print("{:d}: {}".format(i, arg))
