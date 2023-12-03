973acfa114b07fc9ceda65d21864580a92adae43
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-03 14:44:07 -0800

    update 1-ellement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     def element_at(my_list, idx):
-        my_list = [1, 2, 3, 4, 5]
-        idx = 3
+        if idx < 0:
+            return None
+        if idx > len(my_list):
+            return None
         print("Element at {:d} is {}".format(idx, elemet_at(my_list, idx)))
