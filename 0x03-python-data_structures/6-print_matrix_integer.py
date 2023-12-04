96fcf1aabf424c41c3ee2ea9f06ecbabdb411484
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-04 03:30:39 -0800

    update 6-print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        print("{:d}".format(i))
+    if not matrix:
+        print()
+    else:
+        for i in range(len(matrix)):
+            for item in range(len(matrix[i])):
+                if item != len(matrix[i] - 1):
+                    esp = ''
+                else:
+                    esp = ''
+                print("{:d}".format(matrix[i][item]), end=esp)
+        print()
