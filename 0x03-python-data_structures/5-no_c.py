4a7bb182e89b1663e1f41e41a715f4890c48f9d8
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-04 03:00:54 -0800

    update 5-no_c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def no_c(my_string):
-        for my_string in "cC":
-            my_string.remove()
+def no_c(my_string):
+    for my_string in "cC":
+        my_string.remove()
