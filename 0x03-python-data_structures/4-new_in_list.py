991b24fce406e234b80c128de6a9eff8db23a481
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-04 02:45:28 -0800

    update 4-new

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def new_in_list(my_list, idx, element):
-        if idx < 0:
-            return my_list.copy()
-        if idx >= len(my_list):
-            return my_list.copy()
-        my_list[idx] = element
-        return my_list
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list.copy()
+    if idx >= len(my_list):
+        return my_list.copy()
+    my_list[idx] = element
+    return my_list
