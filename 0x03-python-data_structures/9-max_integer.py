cddaa7595bd92e4e8d56e31836e44eeb1378691c
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-04 15:33:42 -0800

    update 9-max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def max_integer(my_list=[]):
-        if my_list == []:
-            return None
-        print("Max: {}".format(int()))
+def max_integer(my_list=[]):
+    if not my_list:
+        my_list = None
+        print("Max: {:d}".format(my_list))
