9976b8ab6143ca3007f3ed34804a02b2e2b6e6a0
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-04 15:26:05 -0800

    update 8-multiple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence  == []:
-        sentence[0] = None
+    if not sentence:
+        sentence = None
+    if sentence:
+        len_s = len(sentence)
     else:
-        print("Length: {:d} - First character: {}".format(len(sentence), sentence[0]))
+        len_s = 0
+    return "Length: {:d} - First character: {}".format(len(sentence), sentence[:1])
