419599f4e1c355032aac68c2c38a9cedce4fc7b4
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-05 02:48:06 -0800

    update

--- 0x02-python-import_modules/magic_calculation_102.py
+++ 0x02-python-import_modules/magic_calculation_102.py
@@ -1,7 +1,7 @@
-#!/usr/bin/pthon3
+#!/usr/bin/python3
 def add(a, b):
     return a + b
 def sub(a, b):
     return a - b
-myfunc = __import__('102-magic_calculation', fromlist=['102-magic_calculation']).102-magic_calculation
-return dis.dis(myfunc)
+    myfunc = __import__('102-magic_calculation', fromlist=['102-magic_calculation'])
+    return dis.dis(myfunc)
