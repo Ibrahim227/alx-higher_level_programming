1f1a34a3383fdd19cf911ce0e21214febefaadd4
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-06 15:46:30 -0800

    update 0-square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = matrix
-    for x in range(1, 10, 3):
+    new_matrix = []
+    for x in range(1, 10):
         new_matrix.append([x**2])
     return new_matrix
+    #matrix.append(new_matrix)
