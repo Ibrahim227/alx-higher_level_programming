cad838ae3f1a83eef512270d9c26e1f45246f959
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-07 15:44:40 -0800

    add 12-roman

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is not str or roman_string is None:
+        return 0
+    else:
+        for num in range(1, 4000):
+            return roman_string(num)
+    return int(num)
