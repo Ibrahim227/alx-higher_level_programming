4f9e0fd6939b5e2bf2c83f25a953a2fab318289a
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-07 03:32:21 -0800

    add 9-multiply

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dir = a_dictionary.copy()
+    new_dict = list(a_dictionary.keys())
+    for x in new_dict:
+        new_dir[x] *= 2
+    return (new_dir)    
+    
