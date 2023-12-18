5d93814c20c1dbf79dfcb11ca94d44a00881a61e
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-18 08:06:43 -0800

    update 0-sfe

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        print("{}: {}".format(my_list[x], my_list.count))
-    except:
-        print("None")
+        print("{:d}".format(x))
+        return x
+    except Exception:
+        return
