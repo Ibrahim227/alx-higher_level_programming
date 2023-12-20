f6f3d78890ff53c71fb24544ff57796cdb958817
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-20 12:22:38 -0800

    update 1-square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
+""" Square module"""
+
+
 class Square:
     """
     This is a class definition
     """
     def __init__(self, size):
+        """ Constructor
+        Args:
+            size: length of side square
+        """
         self.__size = size
 
-    @property
-    def square(self):
-        """
-        This is a function definition os square
-        """
-        return self.__size
-
-
-"""
-This a docstring for 1-square
-It should give an overview
-"""
