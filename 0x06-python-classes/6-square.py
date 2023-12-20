53b4ea2fc011dfdfa831096b94d1626d1735be09
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-20 14:05:01 -0800

    add 6-square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" Square Module"""
+
+
+class Square:
+    """Square class Docstring"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Constructor
+            Args:
+                size (int)
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """get/set the current size size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ retrieve value"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """ docstring function for position"""
+        return (self.__size)
+
+    @position.setter
+    def position(self, value):
+        """Docstring for position function"""
+        if not isinstance(value, int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """calculate the area"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """ print # square"""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size and self.position == 0:
+            print()
