ed16f5ac294327c004da4f0c3dcd0c2247695679
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-20 08:21:32 -0800

    update 0-square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 class Square:
+    """ Define a Class called Square"""
     def __init__(sefl):
+        """Initialize the Class"""
         pass
     def squrare(self):
+        """ Define a classe method square inside the class Square"""
         print(Square().square())
