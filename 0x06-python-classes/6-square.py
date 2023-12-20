f1b744a78f167db8266e32c649937e1422e6f776
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-20 14:10:16 -0800

    update 6-square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,9 +44,6 @@ class Square:
 
     def my_print(self):
         """ print # square"""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
         if self.__size == 0:
             print()
             return
@@ -56,4 +53,3 @@ class Square:
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
-
