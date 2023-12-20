79b723be7a1df1b2fcdef8c032a82dfbf1c68d5d
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-20 12:02:39 -0800

    ad 1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Square:
+    """
+    This is a class definition
+    """
+    def __init__(self, size):
+        self.__size = size
+
+    @property
+    def square(self):
+        """
+        This is a function definition os square
+        """
+        return self.__size
+
+
+"""
+This a docstring for 1-square
+It should give an overview
+"""
