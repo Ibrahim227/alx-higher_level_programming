e39da39802dde9f13c748da8aece117cf5e72101
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2023-12-20 12:45:59 -0800

    add 3-square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Square Module"""
+
+
+class Square:
+    """ Square Docstring"""
+    def __init__(self, size=0):
+        """ Constructor
+        Args:
+            size (int): area as int
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """Docstring for function"""
+        return self.__size**2
