03ee48ecdc5be6c5d77657c6541bf748f63ca9e6
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-09 14:47:46 -0800

    update 1-write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,5 @@
 
 def write_file(filename="", text=""):
     """writes a string to text file """
-    with open("my_first_file.txt", "w", encoding="utf-8") as myfile:
-        myfile.write(text)
+    with open("my_first_file.txt", "w") as myfile:
+        myfile.write("This School is so cool!\n")
