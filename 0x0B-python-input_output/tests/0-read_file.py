8c37be77f3a919b11f6b7300e32541d56b948e49
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-09 14:37:06 -0800

    update

--- 0x0B-python-input_output/tests/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" function Definition"""
-
-
-def read_file(filename=""):
-    """ reads a text file """
-    with open('my_file_0.txt', "r") as f:
-        res = f.read()
-        print(res, end="")
