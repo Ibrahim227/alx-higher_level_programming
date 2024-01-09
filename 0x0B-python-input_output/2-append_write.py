2668b2ede64f45a219ee1c70db047379fcc8c52a
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-09 14:57:24 -0800

    update 2-append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,5 +3,5 @@ def append_write(filename="", text=""):
     """ Appends a string to the end of file """
     with open("file_append.txt", "w") as myfile:
         myfile.write("This School is so cool!\n")
-    with open("file_append.txt", "a") as myfile:
+    with open("file_append.txt", "a+") as myfile:
         myfile.write("This School is so cool!\n")
