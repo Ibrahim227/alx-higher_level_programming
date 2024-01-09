1df3029745de2b5f38dd499582450d6995072e6f
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-09 15:48:19 -0800

    upadate 2-append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""define the function"""
+
+
 def append_write(filename="", text=""):
-    """ Appends a string to the end of file """
-    with open("file_append.txt", "w") as myfile:
-        myfile.write("This School is so cool!\n")
-    with open("file_append.txt", "a+") as myfile:
-        myfile.write("This School is so cool!\n")
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "a", encoding="utf-8") as myfile:
+        return myfile.write(text)
