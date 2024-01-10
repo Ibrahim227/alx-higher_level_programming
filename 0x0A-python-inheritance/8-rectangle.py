ca49898fbd38e55fe426dd29476217b49200ceda
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-10 13:43:41 -0800

    update 8-rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Defines a fuynction"""
+import BaseGeometry
 
-
-class BaseGeometry:
-    """Defines class BaseGeometry"""
+class Rectangle(BaseGeometry):
+    """Defines class Rectangle"""
     def __init__(self, width, height):
         """constructor"""
         self.__width = width
         self.__height = height
         pass
+    super.__init__()
