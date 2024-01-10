c5f3a1644228a749825e5d45274c426ede4d4139
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-10 14:17:23 -0800

    update 8-rectangele

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Defines a fuynction"""
-import BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """Defines class Rectangle"""
     def __init__(self, width, height):
         """constructor"""
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
-        pass
-    super.__init__()
