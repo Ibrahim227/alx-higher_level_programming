d43a1d0bd9fdbee5fefa32fb5da73338da6f6c6d
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-10 13:58:15 -0800

    update 2-issame

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,4 +4,7 @@
 
 def is_same_class(obj, a_class):
     """Returns true for isinstance, otherwise False"""
-    return isinstance(obj, a_class)
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
