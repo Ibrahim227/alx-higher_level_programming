1505a4628a6ccf558555f22ee75c581c64135014
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-10 12:54:43 -0800

    add 2-_isprint

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#/usr/bin/python3
+"""Defines function"""
+
+
+def is_same_class(obj, a_class):
+    """Returns true for isinstance, otherwise False"""
+    while isinstance:
+        return True
+    else:
+        return False
