96e146f32559d7b77279058d1c54347e86c1c8df
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-10 13:23:31 -0800

    add 6-baseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a fuynction"""
+
+
+class BaseGeometry:
+    """Define class BaseGeometry"""
+    def area(self):
+        """Raise an Exception"""
+        raise Exception("area() is not implemented")
