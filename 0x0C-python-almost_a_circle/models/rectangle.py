6251c7196ba02e531a8dda3ef264a2e225367e97
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-13 12:53:08 -0800

    update rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -43,7 +43,9 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if value <= 0:
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
