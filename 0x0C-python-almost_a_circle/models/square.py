a9bba42af596a4022deb4f2f27bc57d2c8ce9bd8
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-14 08:04:46 -0800

    update square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Documentation Modules"""
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    """Defines class Square that inherit from class Rectanglee"""
+    super().__init__(id, x, y, width, height)
+    def __init__(self, size, x=0, y=0, id=None):
+        """Defines the constructor"""
+        self.size = size
+        self.x = x
+        self.y = y
+        self.id = id
