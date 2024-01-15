75c8e809ee3b3c65bcc267a311462ed74aae671b
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-15 12:48:50 -0800

    update test_base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,8 +8,6 @@ class TestBase(unittet.TestCase):
     def test_to_json_string(list_dictionaries):
         """Defines test for function to_json_string"""
         self.assertIsNotNone(list_dictionaries)
-        return json.dumps(list_dictionaries, sort_keys=True))
-
 
 
 
