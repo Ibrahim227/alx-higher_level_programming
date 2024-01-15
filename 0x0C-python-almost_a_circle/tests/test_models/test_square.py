344985ae6af7aebe6b7d0dfa5e0da3d8cbee0cfb
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-15 15:37:23 -0800

    update test_square

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -2,4 +2,11 @@
 import unittest
 from models.square import Square
 
-class 
+
+class TestSquareMethods(unittest.TestCase):
+    """Defines a test class for Square methods"""
+    pass
+
+
+id __name__ == "__main__":
+    unittest.main()
