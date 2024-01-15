7b5cd9a8465e789b28faf088df588788de229a5f
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-15 14:25:54 -0800

    update test_base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -13,11 +13,12 @@ class TestBaseToString(unittest.TestCase):
 
     def test_list_with_dictionaries(self):
         """test list with dict"""
-        list_dictionaries = '
-                        [{'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8},
+        list_dictionaries = "[
+                        {'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8},
                         <class 'dict'>,
                         [{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}],
-                        <class 'str'>]'
+                        <class 'str'>
+                ]"
 
 
 if __name__ == '__main__':
