c2a32cf8c50a886f0948ff446101a049e6bf4e1d
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-15 15:52:08 -0800

    update test_square

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -7,9 +7,11 @@ class TestSquareMethods(unittest.TestCase):
     """Defines a test class for Square methods"""
     pass
 
+
 class TestUpdate(unittest.TestCase):
     """Defines a test class for update func methods"""
     pass
+
 
 class TestToDictionary(unittest.TestCase):
     """Defines a test class for to_dictionary"""
