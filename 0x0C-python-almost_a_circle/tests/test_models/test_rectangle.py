06892f664a5eae739a36e552706196e10e031e5a
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-15 15:51:21 -0800

    update test_rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,25 +7,31 @@ class TestRectangleMethods(unittest.TestCase):
     """Defines a test class for rectangle"""
     pass
 
+
 class TestArea(unittest.TestCase):
     """Defines a test class for Area methods"""
     pass
+
 
 class TestDisplay(unittest.TestCase):
     """Defines a test class for display func method"""
     pass
 
+
 class Test__str__(unittest.TestCase):
     """Defines  a test class for __str__ methos func"""
     pass
+
 
 class TestUpdate(unittes.TestCase):
     """Defines a test class for Update func methods"""
     pass
 
+
 class TestToDictionary(unittest.TestCase):
     """Defines a test class for to_dictionary func method"""
     pass
+
 
 if __name__ == "__main__":
     unittest.main()
