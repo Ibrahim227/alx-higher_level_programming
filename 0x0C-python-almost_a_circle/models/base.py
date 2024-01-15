340c4553ea347073d61210a490ac31ef52b438bd
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-15 15:48:51 -0800

    update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,8 +31,7 @@ class Base:
             list_objs = []
         filename = cls.__name__ + ".json"
         with open(filename, 'w') as f:
-            json_str = \
-                cls.to_json_string([obj.to_dictionary() for obj in list_objs]
+            json_str = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
             f.write(json_str)
 
     @staticmethod
