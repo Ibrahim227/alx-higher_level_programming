ff3046f6ee120b4dcc15953bf3c056d818d79b5a
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-15 12:36:27 -0800

    update test_base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,4 +5,6 @@ import unittest
 
 class TestBase(unittet.TestCase):
     """Define class for Test"""
-    def test_
+    def test_to_json_string(list_dictionaries):
+        """Defines test for function to_json_string"""
+        
