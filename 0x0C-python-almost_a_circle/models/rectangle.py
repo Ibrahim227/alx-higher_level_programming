a1037b18112b47fc69b903108b869aa4bd07a5cf
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-16 13:06:47 -0800

    update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,7 +83,7 @@ class Rectangle(Base):
         """assigns an argument to each attribute"""
         attr = ["id", "width", "height", "x", "y"]
         if args:
-            for i in len(args):
+            for i in range(len(args)):
                 setattr(self, attr[i], args[i])
         elif kwargs:
             for key, value in kwargs.items():
