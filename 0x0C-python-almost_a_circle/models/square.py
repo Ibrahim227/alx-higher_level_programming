19e75a0e1c653785b5f63e6afc1604ef4020ee8b
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-16 13:21:37 -0800

    update square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,9 +39,8 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """returns the dictionary representation of a Square"""
-        return dict({
+        return {
                 'id': self.id,
                 'x': self.x,
                 'size': self.width,
-                'y': self.y
-            })
+                'y': self.y}
