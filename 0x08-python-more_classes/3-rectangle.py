bd63b50c7dba56e4daf887b48c1227a435753d79
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-22 12:58:25 -0800

    add 3-rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -39,8 +39,8 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        if self.__width or self.__height == 0:
+        if (self.__width or self.__height) == 0:
             return '#'
-        if self.__width or self.__height == 0:
+        if (self.__width or self.__heigh) == 0:
             return ""
         #return 2 * (self.__width + self.__height)
