f3f75a0d58d43947007b4ebc133e52a27c02c05d
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-22 12:44:26 -0800

    update 2-rectangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,5 +1,42 @@
 #!/usr/bin/python3
-"""Docstring Docummentation"""
+"""Doctstring"""
 
 
-clas
+class Rectangle:
+    """Defines a class Rectangle"""
+    def __init__(self, width=0, height=0):
+        """Defines the constructor"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return 2 * (self.__width + self.__height)
