24731d786beaf13521926947818eaaf77675aa4d
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-01-23 13:58:55 -0800

    update 6-rectangle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,6 +4,7 @@
 
 class Rectangle:
     """Defines a class Rectangle"""
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         """Defines the constructor"""
         self.__width = width
