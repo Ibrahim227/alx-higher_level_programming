23bfa18b9ba4ff0b397724177743eaf02c0e59c9
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-09 16:26:28 -0800

    update 5-full_table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
 -- prints the full description of the table first_table from the db hbtn_0c_0
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = 'hbtn_0c_0';
+SELECT first_table
+FROM hbtn_0c_0;
