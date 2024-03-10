3e97376cd1a4e661a88b88f3b85d09d7a565a70d
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-09 16:51:33 -0800

    update 9-full-creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- creates a table second_table in the database hbtn_0c_0
 CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table VALUES (
-(id = 1, name = 'John', score = 10),
-(id = 2, name = 'Alex', score = 3),
-(id = 3, name = 'Bob', score = 14)
- (id = 1, name = 'George', score = 8));
+INSERT INTO `second_table`(`id`, `name`, `score`) VALUES (
+(1, "Jhon", 10),
+(2, "Alex"3),
+(3, "Bob", 14)
+ (4, "George", 8));
