ca36978b8dec6363574296ba0ca07a45c4e4c464
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-13 06:27:14 -0700

    update 4-concat

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+
+if (!args.length) {
+  console.log(undefined);
+} else if (args.length === 1) {
+  console.log(`${args}` 'is' `${args}`);
+} else {
+  console.log('Arguments found');
+}
