7e842d00e23564c123d9e3cc47ccb22a67bce898
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-13 07:18:19 -0700

    update 6-multi

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let all = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < all.length; i++) {
-  console.log(i);
+for (let i = 0; i < all[i]; i++) {
+  console.log(all[i]);
 }
