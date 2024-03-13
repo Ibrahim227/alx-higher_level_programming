ca2891550b047b6dde490e49b865d85c15fdf7ca
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-13 09:14:11 -0700

    update 11-second_biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ const args = process.argv.slice(2);
 if (args.length <= 1) {
   console.log(0);
 } else {
-    const integers = args.map(Number).sort((a, b) => b - a);
-    console.log(integers[1]);
+  const integers = args.map(Number).sort((a, b) => b - a);
+  console.log(integers[1]);
 }
