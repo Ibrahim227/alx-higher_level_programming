877dcb28679034ffde11670b0398fce2d4ded5e4
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-13 08:20:23 -0700

    update 10-factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const n = parseInt(process.argv[2]);
 
-function factorial(n) {
+function factorial (n) {
   if (n === 0 || n === 1) {
     return 1;
   } else {
-      return n * factorial(n - 1);
+    return n * factorial(n - 1);
   }
 }
-
+const n = parseInt(process.argv[2]);
 console.log(factorial(n));
