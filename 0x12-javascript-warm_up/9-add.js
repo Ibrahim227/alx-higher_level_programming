976949002b9be3e5e938f411b36931d643278c87
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-13 08:08:08 -0700

    update 9-add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
 
 function add (a, b) {
   return a + b;
 }
 
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
 if (!isNaN(arg1) && !isNaN(arg2)) {
   console.log(add(arg1, arg2));
 }
