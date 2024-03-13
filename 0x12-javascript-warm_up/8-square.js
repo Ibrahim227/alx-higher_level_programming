f71025caaf75a31f23124553b9a0e0f52cabbbe0
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-13 07:48:05 -0700

    update 8-square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,8 +3,10 @@ const arg = parseInt(process.argv[2]);
 
 if (!isNaN(arg)) {
   for (let i = 0; i < arg; i++) {
-    for (let j = 0; j < i; j++)
-      console.log('X');
+    let row = '';
+    for (let j = 0; j < arg; j++) {
+      row += 'X';
+    }console.log(row);
   }
 } else {
   console.log('Missing Size');
