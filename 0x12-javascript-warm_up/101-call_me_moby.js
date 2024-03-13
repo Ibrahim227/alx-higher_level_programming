94a73e91323cb333d7c942dce48bd94da7f7d2b8
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-13 10:10:06 -0700

    update 100-call_mee

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,1 +1,13 @@
+#!/usr/bin/node
 
+function exeTime(x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+function strVar() {
+  console.log("C is fun");
+}
+
+exeTime(x, strVar);
