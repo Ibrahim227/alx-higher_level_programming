ff529c84616048d155b051780a35359286dedd33
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-13 07:33:59 -0700

    update 7-multi

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const arg = process.argv[2];
 
-const number = parseInt(arg);
 const strVar = "C is fun";
 
-for (let i = 0; i < number; i++) {
-  console.log(`${number}` * `${strVar}`);
+if (Number.isInteger(arg)) {
+  for (let i = 0; i < number; i++) {
+    console.log(`${number}` * `${strVar}`);
+  }
+} else {
+    console.log('Missing number of occurrences');
 }
-console.log('Missing number of occurrences');
-
