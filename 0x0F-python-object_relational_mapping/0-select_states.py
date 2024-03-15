8866c60e136be4faef349ad052250cb1523bbaf1
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-15 09:48:50 -0700

    update 0-select_states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import MySQLdb
 
 db = MySQLdb.connect(host="localhost",port=3306,user="root",password="password",db="hbtn_0e_0_usa")
 cur = db.cursor()
-cur.execute("""SELECT * from states""")
+cur.execute("""SELECT * from states WHERE id <= 5""")
 rows = cur.fetchall()
 for row in rows:
     print(row)
