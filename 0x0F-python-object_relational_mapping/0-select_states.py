560356af354593abc0ed86072e767eca5c24d50e
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-15 09:30:59 -0700

    update 0-select_states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
-"""Import the required module"""
-from MySQLdb import _mysql
+##!/usr/bin/python3
+#"""Import the required module"""
+#from MySQLdb import _mysql
 
 
 #db = _mysql.connect(host="localhost",port=3306,user="root",password="password",database="hbtn_0e_0_usa")
 #db.query("""SELECT name from states ORDER BY id""")
-
+-- se
 SHOW DATABASES;
