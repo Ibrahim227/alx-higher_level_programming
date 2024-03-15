e2fbcb630e4677e84e455a203f33d18250a6dab1
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-15 09:38:25 -0700

    update 0-select_states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ from MySQLdb import _mysql
 
 
 db = _mysql.connect(host="localhost",port=3306,user="root",password="password",database="hbtn_0e_0_usa")
-db.query("""SELECT id, name from states""")
+db.query("""SELECT * from states""")
 
 r = db.use_result()
 r.fetch_row()
