924dabe315b0020b5644cb9eecc7420635a69da9
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-15 09:34:19 -0700

    update 0-select_states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,3 +6,5 @@ from MySQLdb import _mysql
 db = _mysql.connect(host="localhost",port=3306,user="root",password="password",database="hbtn_0e_0_usa")
 db.query("""SELECT name from states ORDER BY id""")
 
+r = db.use_result()
+r.fetch_row()
