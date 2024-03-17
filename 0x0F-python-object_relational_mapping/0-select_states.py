8822d145b5423b27c70c9f3d584d726abf6a810e
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-17 15:13:51 -0700

    update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,7 @@
 import MySQLdb
 
 
-db = MySQLdb.connect(host="localhost",port=3306,user="root",password="password",db="hbtn_0e_0_usa")
+db = MySQLdb.connect(user="root",password="password",db="hbtn_0e_0_usa8")
 cur = db.cursor()
 cur.execute("""SELECT id, name from states LIMIT 5""")
 rows = cur.fetchall()
