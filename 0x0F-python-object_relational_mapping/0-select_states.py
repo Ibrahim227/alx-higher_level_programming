2bb3721ec390461445f5670a97b7f55d4b652849
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-17 15:16:46 -0700

    update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import MySQLdb
 
 db = MySQLdb.connect(user="root",password="password",db="hbtn_0e_0_usa")
 cur = db.cursor()
-cur.execute("""SELECT id, name from states LIMIT 5""")
+cur.execute("""SELECT id, name from states ORDER BY states.id LIMIT 5""")
 rows = cur.fetchall()
 for row in rows:
     print(row)
