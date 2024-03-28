11d4ea7ef46f58032fac31972c0266f44e071d3c
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-28 16:20:46 -0700

    update 0-hbtn_header

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Import Required module"""
-import urllib
+import urllib.request
 
 
-req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-pr = urllib.request.urlopen(req)
-print(pr.code)
-print(pr.read)
+if __name__ == '__main__:':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        values = {type: html}
+        print(f"Body response:'\t - :' {values}")
+        print(html)
