c78592b9d06891e96427eec4b1562063fe3efcbf
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-28 16:52:39 -0700

    update 1-hbtn

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     url = str(sys.argv[1])
     m_req = urllib.request.Request(url)
     with urllib.request.urlopen(m_req) as resp:
-        if "X-Request-Id" in m_req.headers:
-            x_id = dict(m_req.header).get("X-Request-Id")
+        if "X-Request-Id" in resp.headers:
+            x_id = dict(resp.header).get("X-Request-Id")
             print(x_id)
