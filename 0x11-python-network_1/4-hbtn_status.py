d2c42013346e068776e2b4fd8b18d40ce1251850
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-28 17:32:26 -0700

    update 4-hbtn_status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Import requires module"""
 import requests
+
+
 my_url = "https://alx-intranet.hbtn.io/status"
 with requests.get(my_url) as my_response:
     if my_response.status_code == 200:
