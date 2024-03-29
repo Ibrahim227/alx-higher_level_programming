95ec9dd0109010f009d152279c60fffc575cced3
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-28 17:05:15 -0700

    update 2-post_email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
     url = str(sys.argv[1])
     my_email = {"email": str(sys.argv[2])}
-    send_data = urllib.parse.urlencode(url, send_data, method="POST")
-    my_req = urllib.request.Request(url)
+    send_data = urllib.parse.urlencode(my_email).encode("ascii")
+    my_req = urllib.request.Request(url, send_data, method="POST")
     with urllib.request.urlopen(my_req) as resp:
-        print(resp.read().decode("utf-8")
+        print(resp.read().decode("utf-8"))
