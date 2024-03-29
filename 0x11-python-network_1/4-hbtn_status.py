59c1e8b8b892e17da5c9da9b072371ff2ba8ad81
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-28 17:35:14 -0700

    update 4-hbtn_status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,8 +7,7 @@ my_url = "https://alx-intranet.hbtn.io/status"
 with requests.get(my_url) as my_response:
     if my_response.status_code == 200:
         print(
-        "Body response:\n"
-        "\t- type: {}\n"
-        "\t- content: {}".format(
-            type(my_response.text),
-            my_response.text))
+            "Body response:\n"
+            "\t- type: {}\n"
+            "\t- content: {}".format(
+                type(my_response.text), my_response.text))
