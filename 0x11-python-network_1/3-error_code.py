21857ab260aa17c1d245d92bf9363154d600af26
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-28 17:22:35 -0700

    update 3-error_code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Imort the required module"""
-import urllib.request
-import urllib.parse
-import urllib.error
+"""import required module"""
 import sys
+import urllib.request
+import urllib.error
+import urllib.parse
 
 
-if __name__ == '__main__':
-    url = str(sys.argv[1])
-    req = urllib.request.Request(url)
+if __name__ == "__main__":
+    my_url = str(sys.argv[1])
+    request = urllib.request.Request(my_url)
     try:
-        with urllib.request.urlopen(url) as resp:
-            resp_r = resp.read()
-            print(resp_r.decode('utf-8'))
+        with urllib.request.urlopen(my_url) as my_response:
+            read_response = my_response.read()
+            print(read_response.decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print("Error Code: {}".format(e.code))
+        print("Error code: {}".format(e.code))
