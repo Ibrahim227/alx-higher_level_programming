f9b0d7e40a00f489c2ce000ce757994717e8abfc
Ibrahim Ibrahim <mamansani.ibrahim01@gmail.com>
2024-03-28 17:13:11 -0700

    update 3-error_code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Imort the required module"""
+import urllib.request
+import urllib.parse
+import urllib.error
+import sys
+
+
+if __name__ == '__main__':
+    url = str(sys.argv[1])
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(url) as resp:
+            resp_r = resp.read()
+            print(resp_r.decode('utf-8')
+    except urllib.error.HTTPError as e:
+        print(f"Error Code: {}".format(e.code))
