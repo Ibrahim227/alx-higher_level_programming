e75d84d941c05442dd4533ae4da2c4f1483427c7
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-04-23 15:29:41 -0700

    update 1-writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,22 +2,11 @@
 
 const fs = require('fs');
 
-function WriteStringToFile (filepath, content) {
-  fs.writeFile(filepath, content + '\n', 'utf-8', (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('');
-    }
-  });
-}
-
-const filepath = process.argv[2];
+const filePath = process.argv[2];
 const content = process.argv[3];
 
-if (!filepath || !content) {
-  console.error('Usage: ./1-writeme.js <filename.txt> <content to write to the file>');
-  process.exit(1);
-}
-
-WriteStringToFile(filepath, content);
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
