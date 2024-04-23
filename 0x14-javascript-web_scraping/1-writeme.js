392c28f136e0f53148be672b44d414d9590b2201
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-04-23 15:08:20 -0700

    update 1-writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 function WriteStringToFile (filepath, content) {
   fs.writeFile(filepath, content + '\n', 'utf-8', (err) => {
     if (err) {
-	  console.error(err);
+      console.error(err);
     } else {
-	  console.log('');
+        console.log('');
     }
   });
 }
