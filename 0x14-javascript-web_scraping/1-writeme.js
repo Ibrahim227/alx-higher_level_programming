7e51bbcd8e820f1d8ef5f7e0f657f70c26ca13cc
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-04-23 14:42:26 -0700

    update 1-writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,1 @@
-#!/usr/bin/node
 
-const fs = require('fs');
-let data = "Python is cool";
-
-fs.writeFile("my_file.txt", data, (err) => {
-    if (err) {
-	console.log(err);
-    } else {
-	console.log("Success")
-    }
-});
