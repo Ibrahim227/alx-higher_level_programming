b011d59bf943e629a66a0246edd45357ba0cea01
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-04-23 15:30:03 -0700

    update 2-status_code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,20 +2,12 @@
 
 const request = require('request');
 
-function displayStatusCode (url) {
-  request.get(url, (error, response) => {
-    if (error) {
-      console.error('Error:', error);
-    } else {
-      console.log('Succcess');
-    }
-  });
-}
-
 const url = process.argv[2];
 
-if (!url) {
-  console.error('Usage: ./2-statuscode <url>');
-}
-
-displayStatusCode(url);
+request(url, (error, response) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
